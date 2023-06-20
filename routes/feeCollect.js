626ae40510e0484825859b90a3f321f4409af56b
ninjahattori21 <115465151+ninjahattori21@users.noreply.github.com>
2023-06-20 20:42:28 +0530

    updated code

--- routes/feeCollect.js
+++ routes/feeCollect.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 const feeCollectController = require('../controllers/feeCollectController')
 const router = require('express').Router()
 
 
-router.get('/:student_id' , feeCollectController.collectStudentFee)
+router.get('/:student_id' , feeCollectController.collectStudentFee);
+router.get('/searchFeePaymentStatus/:payment_id',feeCollectController.status);
 // router.post('/' , FeeDiscountController.createFeeDiscount)
 // router.delete('/:id' , FeeDiscountController.deleteFeeDiscount)
 // router.patch('/:id' , FeeDiscountController.updateFeeDiscount)
@@ -11,14 +11,3 @@ router.get('/:student_id' , feeCollectController.collectStudentFee)
 
 
 module.exports  = router
-=======
-const FeeCategoryController = require("../controllers/feeCollectController");
-const router = require("express").Router();
-
-router.get("/", (req, res) => {
-  console.log("ok");
-});
-router.get("/:student_id", FeeCategoryController.collectStudentFee);
-
-module.exports = router;
->>>>>>> master
