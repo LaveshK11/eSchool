789c4cc8caae3741a11948c60f0e7efc2525ca02
ninjahattori21 <115465151+ninjahattori21@users.noreply.github.com>
2023-07-10 18:44:35 +0530

    updated code

--- controllers/BookListController.js
+++ controllers/BookListController.js
@@ -1,97 +1,85 @@
-const BookList = require('../models/BookList')
-const ApiFactory = require('../utils/apiFactory')
+const BookList = require("../models/BookList");
+const ApiFactory = require("../utils/apiFactory");
 // const BookIssue = require('../models/BookIssue')
-const AppError = require('../utils/AppError')
-const BookIssue = require('../models/IssueBook')
-
+const AppError = require("../utils/AppError");
+const BookIssue = require("../models/IssueBook");
 
 exports.getAllBooks = async (req, res, next) => {
   try {
-
-    let data = await BookList.findAll()
+    let data = await BookList.findAll();
     res.status(200).json({
-      status: 'success',
-      data
-    })
-
+      status: "success",
+      data,
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
-
+};
 
 exports.getBook = async (req, res, next) => {
   try {
-
-    let data = await BookList.findByPk(req.params.id)
+    let data = await BookList.findByPk(req.params.id);
     res.status(200).json({
-      status: 'success',
-      data
-    })
-
+      status: "success",
+      data,
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 exports.getAllStudentBooks = async (req, res, next) => {
   try {
-
     let data = await BookIssue.findAll({
       where: {
         student_id: req.params.student_id,
-        is_returned: false
+        is_returned: false,
       },
       include: {
-        model: BookList
-      }
-    })
+        model: BookList,
+      },
+    });
 
     res.status(200).json({
-      status: 'success',
-      data
-    })
-
+      status: "success",
+      data,
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 exports.issueBook = async (req, res, next) => {
   try {
-
-    
-
-    if (!req.body.book_id)
-      return next(new AppError('Book is required', 404))
+    if (!req.body.book_id) return next(new AppError("Book is required", 404));
 
     if (!req.body.student_id)
-      return next(new AppError('Student is required', 404))
+      return next(new AppError("Student is required", 404));
 
-      if (!req.body.due_return_date)
-      return next(new AppError('Due date is required', 404))
+    if (!req.body.due_return_date)
+      return next(new AppError("Due date is required", 404));
 
-      let book = await BookList.findByPk(req.body.book_id)
+    let book = await BookList.findByPk(req.body.book_id);
 
-      if(book.getDataValue('qty')!=null && book.getDataValue('qty') <= 0)
-      return next(new AppError('Not Available!',404))
-      
+    if (book.getDataValue("qty") != null && book.getDataValue("qty") <= 0)
+      return next(new AppError("Not Available!", 404));
 
-      await BookIssue.create(req.body)
-      
+    await BookIssue.create(req.body);
 
-      res.status(200).json({
-      status: 'success',
-      message: 'Book issued successfully!'
-    })
+    res.status(200).json({
+      status: "success",
+      message: "Book issued successfully!",
+    });
 
-    if(book.getDataValue('qty')!=null)
-    await BookList.decrement('qty',{by:1 ,where:{id:req.body.book_id}})
-
+    if (book.getDataValue("qty") != null)
+      await BookList.decrement("qty", {
+        by: 1,
+        where: { id: req.body.book_id },
+      });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 exports.returnBook = async (req, res, next) => {
   try {
@@ -100,31 +88,48 @@ exports.returnBook = async (req, res, next) => {
     let mm = today.getMonth() + 1; // Months start at 0!
     let dd = today.getDate();
 
-    if (dd < 10) dd = '0' + dd;
-    if (mm < 10) mm = '0' + mm;
+    if (dd < 10) dd = "0" + dd;
+    if (mm < 10) mm = "0" + mm;
 
-    const formattedToday = yyyy + '/' + mm + '/' + dd;
-    
-    if(req.body.return_date)
-    formattedToday = req.body.return_date
+    const formattedToday = yyyy + "/" + mm + "/" + dd;
 
-    await BookIssue.update({ is_returned: true, return_date:formattedToday}, { where: { id: req.params.id } })
+    if (req.body.return_date) formattedToday = req.body.return_date;
 
-    let book = await BookIssue.findOne({id:req.params.id})
+    await BookIssue.update(
+      { is_returned: true, return_date: formattedToday },
+      { where: { id: req.params.id } }
+    );
 
-    await BookList.increment('qty',{by:1,where:{id:book.getDataValue('book_id')}})
+    let book = await BookIssue.findOne({ id: req.params.id });
 
+    await BookList.increment("qty", {
+      by: 1,
+      where: { id: book.getDataValue("book_id") },
+    });
 
     res.status(200).json({
-      status:'success',
-      message:'book returned !'
-    })
-
+      status: "success",
+      message: "book returned !",
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
-exports.createBook = ApiFactory.create(BookList)
-exports.deleteBook = ApiFactory.delete(BookList)
-exports.updateBook = ApiFactory.update(BookList)
+exports.createBook = ApiFactory.create(BookList);
+exports.deleteBook = async (req, res) => {
+  try {
+    await BookList.destroy({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json({
+      status: "success",
+      message: "data deleted",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+exports.updateBook = ApiFactory.update(BookList);
